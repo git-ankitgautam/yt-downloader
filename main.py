1de88e7cf202f73a97f92ae4ef83911dcb21911d
git-ankitgautam <ankitgautam485@gmail.com>
2024-09-21 14:24:55 +0530

    brought code from previous project

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-import yt-dlp
+import yt_dlp
+import os
+from sys import argv
+   
 
+def create_options():
+   options = {}
+   if argv[2] == '--video':
+      options.update({'format' : 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b'})
+   elif argv[2] == '--audio':
+      options.update({'format' : 'm4a/bestaudio/best'})
+
+   return  options
+
+try:
+   link = argv[1]
+   options = create_options()
+except:
+   options = {'format' : 'bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b'}
+   link = input("Enter the link: ")
+   print("custom options not loaded")
+
+with yt_dlp.YoutubeDL(options) as ydl:
+   ydl.download([link])
+
+print('video successfully downloaded')
